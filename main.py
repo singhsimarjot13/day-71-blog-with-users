3cacff82db93f7df9bdd69770e218af40a796152
waheguruji <abcd@gmail.com>
2024-06-22 00:14:25 +0530

    some changes in header etc

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 import  sqlalchemy.exc
 # Import your forms from the forms.py
 from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
@@ -29,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 login_=LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRETKEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 login_.init_app(app)
@@ -47,7 +48,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
